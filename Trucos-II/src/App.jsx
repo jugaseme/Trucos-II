ced229d6bd6c219d0d6f85beb6bdcf09ded1847d
jugaseme <sebastiangarciamedina26@gmail.com>
2026-03-08 21:46:30 -0500

    estilos

--- Trucos-II/src/App.jsx
+++ Trucos-II/src/App.jsx
@@ -4,15 +4,16 @@ import './App.css'
 function App() {
 const [saldo, setSaldo] = useState(0)
 const [valor, setValor] = useState(0)
-
+const [NumBarber , setNumBarber] = useState(0)
 
 
   return (
     <>
-    <div>
+    <div className="App">
       <h1>Trucos II</h1>
 
       <h2>Saldo: ${saldo.toLocaleString("es-Co") }</h2>
+
       <input value={valor} onChange={(e) => setValor(Number(e.target.value))} />
       <button onClick={() => setSaldo(saldo + valor)}>Depositar</button>
       <button onClick={() => setSaldo(saldo - valor)}>Retirar</button>
