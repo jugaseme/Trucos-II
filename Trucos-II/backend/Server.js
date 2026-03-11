b5a50bcc793b99741f52706b6ac65493c8db671b
jugaseme <sebastiangarciamedina26@gmail.com>
2026-03-11 01:21:47 -0500

    post

--- Trucos-II/backend/Server.js
+++ Trucos-II/backend/Server.js
@@ -4,7 +4,7 @@ const express = require("express")
 const cors = require("cors")
 
 const connectDB = require("./config/db")
-const usuariosRoutes = require("./routes/usuarios")
+const usuariosRoutes = require("./routes/usuarios.js")
 
 const app = express()
 
